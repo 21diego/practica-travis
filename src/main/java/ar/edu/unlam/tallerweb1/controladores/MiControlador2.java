4792f727407ecaa1b66fc4134cb61f1d830cde46
KevinVG07 <kevinvg08@gmail.com>
2020-04-29 09:21:56 -0300

    Se configura notificacion por email y se agrega lo de Path Variable

--- src/main/java/ar/edu/unlam/tallerweb1/controladores/MiControlador2.java
+++ src/main/java/ar/edu/unlam/tallerweb1/controladores/MiControlador2.java
@@ -3,19 +3,20 @@ package ar.edu.unlam.tallerweb1.controladores;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class MiControlador {
+public class MiControlador2 {
 
-	@RequestMapping(path="/saludar", method = RequestMethod.GET)
-	public ModelAndView saludar(@RequestParam("nombre") String nombre, @RequestParam("apellido") String apellido) {
+	@RequestMapping(value = {"/saludar2", "/saludar2/{nombre}/{apellido}"})
+	public ModelAndView saludar(@PathVariable String nombre, @PathVariable String apellido) {
 		ModelMap model = new ModelMap();
 		model.put("mostrarNombre", nombre);
 		model.put("mostrarApellido", apellido);
-		return new ModelAndView("saludo", model);
+		return new ModelAndView("saludo2", model);
 	}
 }
---
+++ src/main/webapp/WEB-INF/vistas/saludo2.jsp
@@ -0,0 +1,3 @@
+Tu nombre es: ${mostrarNombre} <br>
+Tu apellido es: ${mostrarApellido} <br>
+Hola ${mostrarNombre} ${mostrarApellido}!
